fce6d63a5e0c6cffbdbc9d90d3d4e651c258d6c3
Yoshizawa Koichi <kyoshizawa@minna-systems.co.jp>
2025-06-02 22:29:35 +0900

    feat: ユーザー登録処理

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+import { authRepository } from '@/modules/auth/auth.repository';
+
 function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      const user = await authRepository.signup(name, email, password);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center">
@@ -17,6 +33,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setName(e.target.value)}
                     id="username"
                     name="username"
                     placeholder="ユーザー名"
@@ -35,6 +52,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setEmail(e.target.value)}
                     id="email"
                     name="email"
                     placeholder="メールアドレス"
@@ -53,6 +71,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setPassword(e.target.value)}
                     id="password"
                     name="password"
                     placeholder="パスワード"
@@ -63,7 +82,16 @@ function Signup() {
                 </div>
               </div>
               <div>
-                <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 disabled:opacity-50 disabled:cursor-not-allowed">
+                <button
+                  disabled={
+                    name === '' ||
+                    email === '' ||
+                    password === '' ||
+                    !email.includes('@')
+                  }
+                  onClick={handleSignup}
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
                   登録
                 </button>
               </div>
